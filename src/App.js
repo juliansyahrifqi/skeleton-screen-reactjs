8546ce8bed54ec1e9ab962af5e976a7c9aded6ac
Rifqi Pratama <oyeaaa@localhost.localdomain>
2021-03-10 20:16:44 +0700

    update readme and add github link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header>
         <h2>Hashnode API</h2>
+        <a href="https://github.com/juliansyahrifqi/hashnode-api-reactjs">Source Code</a>
       </header>
       <div className="post">
         <BlogPost />
